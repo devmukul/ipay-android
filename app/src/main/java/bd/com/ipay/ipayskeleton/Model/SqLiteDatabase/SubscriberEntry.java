aed7ab829f5ea6ed77228082d42961fe778a1482
Rafi Kamal <rafikamal93@gmail.com>
2016-03-01 12:42:29 +0600

    reverted changes

--- app/src/main/java/bd/com/ipay/ipayskeleton/Model/SqLiteDatabase/SubscriberEntry.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/Model/SqLiteDatabase/SubscriberEntry.java
@@ -1,22 +1,9 @@
 package bd.com.ipay.ipayskeleton.Model.SqLiteDatabase;
 
-import com.j256.ormlite.field.DatabaseField;
-import com.j256.ormlite.table.DatabaseTable;
-
-import bd.com.ipay.ipayskeleton.DatabaseHelper.DBConstants;
-
-@DatabaseTable(tableName = DBConstants.DB_TABLE_SUBSCRIBERS)
 public class SubscriberEntry {
-    @DatabaseField(generatedId = true)
-    private long _id;
 
-    @DatabaseField(columnName = DBConstants.KEY_MOBILE_NUMBER)
     private String mobileNumber;
-
-    @DatabaseField(columnName = DBConstants.KEY_NAME)
     private String name;
-
-    public SubscriberEntry() {}
 
     public SubscriberEntry(String mobileNumber, String name) {
         this.mobileNumber = mobileNumber;
