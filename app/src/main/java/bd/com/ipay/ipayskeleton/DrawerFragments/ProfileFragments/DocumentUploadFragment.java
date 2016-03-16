30eda10050672801f25e9dbe84682e676415aee2
Rafi Kamal <rafikamal93@gmail.com>
2016-03-16 17:08:44 +0600

    added document upload fragment

--- app/src/main/java/bd/com/ipay/ipayskeleton/DrawerFragments/ProfileFragments/DocumentUploadFragment.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/DrawerFragments/ProfileFragments/DocumentUploadFragment.java
@@ -1,6 +1,21 @@
 package bd.com.ipay.ipayskeleton.DrawerFragments.ProfileFragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import bd.com.ipay.ipayskeleton.R;
 
 public class DocumentUploadFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_document_upload, container, false);
+
+
+        return v;
+    }
 }
