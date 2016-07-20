e1ecec662bed60749b5da467d8a6cc52c737d565
Reaz Murshed <reaz.murshed.masud@gmail.com>
2016-07-20 17:12:23 +0600

    Unused import removed

--- app/src/main/java/bd/com/ipay/ipayskeleton/Activities/EventActivity.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/Activities/EventActivity.java
@@ -1,11 +1,9 @@
 package bd.com.ipay.ipayskeleton.Activities;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import bd.com.ipay.ipayskeleton.EventFragments.CreateNewEventFragment;
