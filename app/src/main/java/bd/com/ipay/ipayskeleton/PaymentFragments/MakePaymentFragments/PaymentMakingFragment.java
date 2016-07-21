0fae9e15a1983243877369bb0960bf2d118ba6bb
Shetu <farzana.ahamed@ipay.com.bd>
2016-07-21 17:28:30 +0600

    button hidden in make payment review dialog, pay by qr code bug fixed, Qr code scaning bug fixed in send money

--- app/src/main/java/bd/com/ipay/ipayskeleton/PaymentFragments/MakePaymentFragments/PaymentMakingFragment.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/PaymentFragments/MakePaymentFragments/PaymentMakingFragment.java
@@ -150,10 +150,9 @@ public class PaymentMakingFragment extends Fragment implements HttpResponseListe
                 mHandler.post(new Runnable() {
                     @Override
                     public void run() {
-                        if (result.matches("[0-9]+") && result.length() > 2) {
-                            ContactEngine.formatMobileNumberBD(result);
-                            mMobileNumberEditText.setText("+" + result);
-                        } else if (getActivity() != null)
+                        if (ContactEngine.isValidNumber(result)) {
+                            mMobileNumberEditText.setText(ContactEngine.formatMobileNumberBD(result));
+                        }  else if (getActivity() != null)
                             Toast.makeText(getActivity(), getResources().getString(
                                     R.string.please_scan_a_valid_pin), Toast.LENGTH_SHORT).show();
 
