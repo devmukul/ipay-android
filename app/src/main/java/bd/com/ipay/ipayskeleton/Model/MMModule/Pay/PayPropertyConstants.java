3ade88bf01f4e865760fb28c82605ba8efde5ec4
maliha.sarwat <maliha.sarwat@ipay.com.bd>
2016-07-26 15:55:31 +0600

    Pay Fragment is modified, Icon added

--- app/src/main/java/bd/com/ipay/ipayskeleton/Model/MMModule/Pay/PayPropertyConstants.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/Model/MMModule/Pay/PayPropertyConstants.java
@@ -13,9 +13,9 @@ public class PayPropertyConstants {
     public static final HashMap<String, Integer> PAY_PROPERTY_NAME_TO_ICON_MAP = new HashMap<>();
 
     static {
-        PAY_PROPERTY_NAME_TO_ICON_MAP.put(TOP_UP, R.drawable.ic_photoid);
-        PAY_PROPERTY_NAME_TO_ICON_MAP.put(MAKE_PAYMENT, R.drawable.ic_photoid);
-        PAY_PROPERTY_NAME_TO_ICON_MAP.put(EDUCATION, R.drawable.ic_parent);
+        PAY_PROPERTY_NAME_TO_ICON_MAP.put(TOP_UP, R.drawable.ic_top_up);
+        PAY_PROPERTY_NAME_TO_ICON_MAP.put(MAKE_PAYMENT, R.drawable.ic_make_payment);
+        PAY_PROPERTY_NAME_TO_ICON_MAP.put(EDUCATION, R.drawable.ic_education);
         PAY_PROPERTY_NAME_TO_ICON_MAP.put(CREATE_INVOICE, R.drawable.ic_parent);
     }
 }
