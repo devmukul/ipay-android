b0d566dc2a879cf51f6eb7800bbf7afcd04fe2cc
Rafi Kamal <rafikamal93@gmail.com>
2016-07-28 17:33:37 +0600

    add friend implemented

--- app/src/main/java/bd/com/ipay/ipayskeleton/HomeFragments/ContactsFragments/ContactsHolderFragment.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/HomeFragments/ContactsFragments/ContactsHolderFragment.java
@@ -1,25 +1,41 @@
 package bd.com.ipay.ipayskeleton.HomeFragments.ContactsFragments;
 
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.flipboard.bottomsheet.BottomSheetLayout;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import bd.com.ipay.ipayskeleton.Api.GetFriendsAsyncTask;
 import bd.com.ipay.ipayskeleton.Api.HttpRequestGetAsyncTask;
+import bd.com.ipay.ipayskeleton.Api.HttpRequestPostAsyncTask;
 import bd.com.ipay.ipayskeleton.Api.HttpResponseListener;
 import bd.com.ipay.ipayskeleton.Api.HttpResponseObject;
+import bd.com.ipay.ipayskeleton.Model.Friend.AddFriendRequest;
+import bd.com.ipay.ipayskeleton.Model.Friend.AddFriendResponse;
+import bd.com.ipay.ipayskeleton.Model.Friend.InfoAddFriend;
 import bd.com.ipay.ipayskeleton.Model.MMModule.Profile.IntroductionAndInvite.GetInviteInfoResponse;
 import bd.com.ipay.ipayskeleton.R;
 import bd.com.ipay.ipayskeleton.Utilities.Constants;
+import bd.com.ipay.ipayskeleton.Utilities.ContactEngine;
 import bd.com.ipay.ipayskeleton.Utilities.Utilities;
 
 public class ContactsHolderFragment extends Fragment implements HttpResponseListener {
@@ -35,12 +51,23 @@ public class ContactsHolderFragment extends Fragment implements HttpResponseList
     private ContactsFragment miPayMemberContactsFragment;
 
     private TextView mContactCount;
+    private FloatingActionButton mAddContactButton;
+
+    private HttpRequestPostAsyncTask mAddFriendAsyncTask;
+    private AddFriendResponse mAddFriendResponse;
+
+    private EditText nameView;
+    private EditText mobileNumberView;
+
+    private ProgressDialog mProgressDialog;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_contact_holder, container, false);
+
         mBottomSheetLayout = (BottomSheetLayout) v.findViewById(R.id.bottom_sheet);
         mContactCount = (TextView) v.findViewById(R.id.contact_count);
+        mAddContactButton = (FloatingActionButton) v.findViewById(R.id.fab_add_contact);
 
         mAllContactsSelector = (Button) v.findViewById(R.id.button_contacts_all);
         miPayContactsSelector = (Button) v.findViewById(R.id.button_contacts_ipay);
@@ -60,6 +87,15 @@ public class ContactsHolderFragment extends Fragment implements HttpResponseList
                 switchToiPayContacts();
             }
         });
+
+        mAddContactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAddFriendDialog();
+            }
+        });
+
+        mProgressDialog = new ProgressDialog(getActivity());
 
         getInviteInfo();
 
@@ -83,6 +119,90 @@ public class ContactsHolderFragment extends Fragment implements HttpResponseList
             button.setBackgroundResource(R.drawable.background_contact_selector_inactive);
             button.setTextColor(ContextCompat.getColor(getActivity(), R.color.colorTextPrimary));
         }
+    }
+
+    private void showAddFriendDialog() {
+        MaterialDialog.Builder dialog = new MaterialDialog.Builder(getActivity());
+        dialog
+                .title(R.string.add_a_friend)
+                .autoDismiss(false)
+                .customView(R.layout.dialog_add_friend, false)
+                .positiveText(R.string.add)
+                .negativeText(R.string.cancel);
+
+        View dialogView = dialog.build().getCustomView();
+
+        nameView = (EditText) dialogView.findViewById(R.id.edit_text_name);
+        mobileNumberView = (EditText) dialogView.findViewById(R.id.edit_text_mobile_number);
+
+        dialog.onPositive(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        if (verifyUserInputs()) {
+                            mProgressDialog.setMessage(getString(R.string.progress_dialog_adding_friend));
+
+                            addFriend(nameView.getText().toString(), mobileNumberView.getText().toString());
+
+                            Utilities.hideKeyboard(getActivity(), nameView);
+                            Utilities.hideKeyboard(getActivity(), mobileNumberView);
+
+                            dialog.dismiss();
+                        }
+
+                    }
+                });
+
+        dialog.onNegative(new MaterialDialog.SingleButtonCallback() {
+            @Override
+            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                Utilities.hideKeyboard(getActivity(), nameView);
+                Utilities.hideKeyboard(getActivity(), mobileNumberView);
+
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+        nameView.requestFocus();
+
+    }
+
+    private boolean verifyUserInputs() {
+
+        boolean error = false;
+
+        String name = nameView.getText().toString();
+        String mobileNumber = mobileNumberView.getText().toString();
+
+        if (name.isEmpty()) {
+            nameView.setError(getString(R.string.error_invalid_name));
+            error = true;
+        }
+
+        if (!ContactEngine.isValidNumber(mobileNumber)) {
+            mobileNumberView.setError(getString(R.string.error_invalid_mobile_number));
+            error = true;
+        }
+
+        return !error;
+    }
+
+    private void addFriend(String name, String phoneNumber) {
+        if (mAddFriendAsyncTask != null) {
+            return;
+        }
+
+        List<InfoAddFriend> newFriends = new ArrayList<>();
+        newFriends.add(new InfoAddFriend(ContactEngine.formatMobileNumberBD(phoneNumber), name));
+
+        AddFriendRequest addFriendRequest = new AddFriendRequest(newFriends);
+        Gson gson = new Gson();
+        String json = gson.toJson(addFriendRequest);
+
+        mAddFriendAsyncTask = new HttpRequestPostAsyncTask(Constants.COMMAND_ADD_FRIENDS,
+                Constants.BASE_URL_FRIEND + Constants.URL_ADD_FRIENDS, json, getActivity(), this);
+        mAddFriendAsyncTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+
     }
 
     private void getInviteInfo() {
@@ -152,22 +272,54 @@ public class ContactsHolderFragment extends Fragment implements HttpResponseList
 
     @Override
     public void httpResponseReceiver(HttpResponseObject result) {
+        if (isAdded()) {
+            mProgressDialog.dismiss();
+        }
 
-        try {
-            Gson gson = new Gson();
+        if (result == null || result.getStatus() == Constants.HTTP_RESPONSE_STATUS_INTERNAL_ERROR
+                || result.getStatus() == Constants.HTTP_RESPONSE_STATUS_NOT_FOUND) {
+            mGetInviteInfoTask = null;
+            mAddFriendAsyncTask = null;
+            if (getContext() != null)
+                Toast.makeText(getContext(), R.string.service_not_available, Toast.LENGTH_LONG).show();
+            return;
+        }
 
-            if (result.getApiCommand().equals(Constants.COMMAND_GET_INVITE_INFO)) {
+        Gson gson = new Gson();
+
+        if (result.getApiCommand().equals(Constants.COMMAND_GET_INVITE_INFO)) {
+            if (result.getStatus() == Constants.HTTP_RESPONSE_STATUS_OK) {
                 try {
                     ContactsHolderFragment.mGetInviteInfoResponse = gson.fromJson(result.getJsonString(), GetInviteInfoResponse.class);
-
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-                mGetInviteInfoTask = null;
             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
+            mGetInviteInfoTask = null;
+
+        } else if (result.getApiCommand().equals(Constants.COMMAND_ADD_FRIENDS)) {
+            try {
+                mAddFriendResponse = gson.fromJson(result.getJsonString(), AddFriendResponse.class);
+                if (result.getStatus() == Constants.HTTP_RESPONSE_STATUS_OK) {
+
+                    if (getActivity() != null) {
+                        Toast.makeText(getActivity(), R.string.add_friend_successful, Toast.LENGTH_LONG).show();
+                        new GetFriendsAsyncTask(getActivity()).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+                    }
+                } else {
+                    if (getActivity() != null)
+                        Toast.makeText(getActivity(), R.string.failed_add_friend, Toast.LENGTH_LONG).show();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+
+                if (getActivity() != null)
+                    Toast.makeText(getActivity(), R.string.failed_add_friend, Toast.LENGTH_LONG).show();
+            }
+
+            mAddFriendAsyncTask = null;
         }
+
     }
 }
