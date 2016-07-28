fcc317ed6ae246142c2d41314f0e152e2d544c97
maliha.sarwat <maliha.sarwat@ipay.com.bd>
2016-07-28 19:23:40 +0600

    Flow is modified

--- app/src/main/java/bd/com/ipay/ipayskeleton/Utilities/Constants.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/Utilities/Constants.java
@@ -121,14 +121,14 @@ public class Constants {
      * <p/>
      * *** Set it to false if you are not using the default password ***
      */
-    public static final boolean AUTO_LOGIN = true;
+    public static final boolean AUTO_LOGIN = false;
 
     // Server Type 1 -> dev server
     // Server Type 2 -> test server
     // Server Type 3 -> stage server
     // Server Type 4 -> live server
     // Server Type 5 -> local server
-    public static final int SERVER_TYPE = 2;
+    public static final int SERVER_TYPE = 1;
 
     public static final String BASE_URL_MM;
     public static final String BASE_URL_SM;
