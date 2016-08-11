28827881863030a24898cfe1fb3f7d5bdb17c1b1
maliha.sarwat <maliha.sarwat@ipay.com.bd>
2016-08-11 11:06:42 +0600

    Otp pages are redesigned

--- app/src/main/java/bd/com/ipay/ipayskeleton/LoginAndSignUpFragments/PersonalSignUpFragments/OTPVerificationPersonalFragment.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/LoginAndSignUpFragments/PersonalSignUpFragments/OTPVerificationPersonalFragment.java
@@ -52,7 +52,7 @@ public class OTPVerificationPersonalFragment extends Fragment implements HttpRes
     private LoginResponse mLoginResponseModel;
 
     private Button mActivateButton;
-    private TextView mResendOTPButton;
+    private Button mResendOTPButton;
     private EditText mOTPEditText;
     private TextView mTimerTextView;
 
@@ -72,7 +72,7 @@ public class OTPVerificationPersonalFragment extends Fragment implements HttpRes
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_otp_verification, container, false);
         mActivateButton = (Button) v.findViewById(R.id.buttonVerifyOTP);
-        mResendOTPButton = (TextView) v.findViewById(R.id.buttonResend);
+        mResendOTPButton = (Button) v.findViewById(R.id.buttonResend);
         mTimerTextView = (TextView) v.findViewById(R.id.txt_timer);
         mOTPEditText = (EditText) v.findViewById(R.id.otp_edittext);
 
