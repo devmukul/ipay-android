6752dfacb402c0843fc59d5f353922470c89d663
maliha.sarwat <maliha.sarwat@ipay.com.bd>
2016-08-18 11:03:32 +0600

    Ui is modified

--- app/src/main/java/bd/com/ipay/ipayskeleton/Model/MMModule/Profile/ProfileCompletion/ProfileCompletionPropertyConstants.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/Model/MMModule/Profile/ProfileCompletion/ProfileCompletionPropertyConstants.java
@@ -68,7 +68,7 @@ public class ProfileCompletionPropertyConstants {
         PROPERTY_NAME_TO_ICON_MAP.put(BUSINESS_INFO, R.drawable.ic_basicinfo_profile);
         PROPERTY_NAME_TO_ICON_MAP.put(PERSONAL_ADDRESS, R.drawable.ic_address_profile);
         PROPERTY_NAME_TO_ICON_MAP.put(BUSINESS_ADDRESS, R.drawable.ic_signup_add1);
-        PROPERTY_NAME_TO_ICON_MAP.put(PROFILE_PICTURE, R.drawable.ic_profilepic);
+        PROPERTY_NAME_TO_ICON_MAP.put(PROFILE_PICTURE, R.drawable.ic_signup_personal);
         PROPERTY_NAME_TO_ICON_MAP.put(PHOTOID, R.drawable.ic_photoid);
         PROPERTY_NAME_TO_ICON_MAP.put(VERIFICATION_DOCUMENT, R.drawable.ic_doc);
         PROPERTY_NAME_TO_ICON_MAP.put(BUSINESS_DOCUMENTS, R.drawable.ic_photoid);
