593a5c99c9678e4aadc14640ca31b7bad6e9ced5
maliha.sarwat <maliha.sarwat@ipay.com.bd>
2016-10-03 17:05:44 +0600

    Primary Email header is hidden in case no primary email. Email List Ui is modified

--- app/src/main/java/bd/com/ipay/ipayskeleton/ProfileFragments/EmailFragment.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/ProfileFragments/EmailFragment.java
@@ -82,7 +82,8 @@ public class EmailFragment extends ProgressFragment implements HttpResponseListe
     private ProgressDialog mProgressDialog;
 
     private TextView mPrimaryEmailView;
-    private TextView mOtherEmailView;
+    private TextView mPrimaryEmailViewHeader;
+    private TextView mOtherEmailViewHeader;
     private ImageView mPrimaryVerificationStatus;
 
     private TextView mEmptyListTextView;
@@ -103,7 +104,8 @@ public class EmailFragment extends ProgressFragment implements HttpResponseListe
         getActivity().setTitle(R.string.email);
 
         mPrimaryEmailView = (TextView) v.findViewById(R.id.textview_email);
-        mOtherEmailView = (TextView) v.findViewById(R.id.other_email_header);
+        mPrimaryEmailViewHeader = (TextView) v.findViewById(R.id.primary_email_header);
+        mOtherEmailViewHeader = (TextView) v.findViewById(R.id.other_email_header);
         mPrimaryVerificationStatus = (ImageView) v.findViewById(R.id.email_verification_status);
         mEmailListRecyclerView = (RecyclerView) v.findViewById(R.id.list_email);
         mSwipeRefreshLayout = (SwipeRefreshLayout) v.findViewById(R.id.swipe_refresh_layout);
@@ -408,16 +410,22 @@ public class EmailFragment extends ProgressFragment implements HttpResponseListe
             else mEmptyListTextView.setVisibility(View.GONE);
 
             if (mEmails.size() > 0 && mEmails.get(0).isPrimary()) {
+                mPrimaryEmailViewHeader.setVisibility(View.VISIBLE);
+                mPrimaryEmailView.setVisibility(View.VISIBLE);
+                mPrimaryVerificationStatus.setVisibility(View.VISIBLE);
                 mPrimaryEmailView.setText(mEmails.get(0).getEmailAddress());
                 mEmails.remove(0);
+
             } else {
-                mPrimaryEmailView.setText(R.string.not_set_yet);
-                mPrimaryVerificationStatus.setVisibility(View.INVISIBLE);
+                mPrimaryEmailViewHeader.setVisibility(View.GONE);
+                mPrimaryEmailView.setVisibility(View.GONE);
+                mPrimaryVerificationStatus.setVisibility(View.GONE);
             }
+
             if (mEmails.size() > 0)
-                mOtherEmailView.setVisibility(View.VISIBLE);
+                mOtherEmailViewHeader.setVisibility(View.VISIBLE);
             else
-                mOtherEmailView.setVisibility(View.GONE);
+                mOtherEmailViewHeader.setVisibility(View.GONE);
 
             setContentShown(true);
 
