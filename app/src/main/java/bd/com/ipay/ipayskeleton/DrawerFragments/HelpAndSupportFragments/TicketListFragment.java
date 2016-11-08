be3910182640435711e1412325085d55b19d75e7
maliha.sarwat <maliha.sarwat@ipay.com.bd>
2016-11-08 15:51:55 +0600

    Status code 406 is handled in Ticket list

--- app/src/main/java/bd/com/ipay/ipayskeleton/DrawerFragments/HelpAndSupportFragments/TicketListFragment.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/DrawerFragments/HelpAndSupportFragments/TicketListFragment.java
@@ -160,7 +160,8 @@ public class TicketListFragment extends ProgressFragment implements HttpResponse
                         } else
                             mEmptyListTextView.setVisibility(View.VISIBLE);
 
-                    } else if (result.getStatus() == Constants.HTTP_RESPONSE_STATUS_NOT_FOUND || result.getStatus() == Constants.HTTP_RESPONSE_STATUS_NOT_ACCEPTABLE) {
+                    } else if (result.getStatus() == Constants.HTTP_RESPONSE_STATUS_NOT_FOUND ||
+                            result.getStatus() == Constants.HTTP_RESPONSE_STATUS_NOT_ACCEPTABLE) {
                         mEmptyListTextView.setVisibility(View.VISIBLE);
 
                     } else {
