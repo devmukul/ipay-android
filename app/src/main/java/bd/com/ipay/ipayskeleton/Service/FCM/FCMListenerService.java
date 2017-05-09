b405ddf620d103bf7258c7c6bcb449f690a04fa6
maliha.sarwat <maliha.sarwat@ipay.com.bd>
2017-05-09 17:13:51 +0600

    IMD-1074 - Notification  Icon load from URL is added

--- app/src/main/java/bd/com/ipay/ipayskeleton/Service/FCM/FCMListenerService.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/Service/FCM/FCMListenerService.java
@@ -1,12 +1,7 @@
 package bd.com.ipay.ipayskeleton.Service.FCM;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.support.v4.app.NotificationCompat;
+import android.os.AsyncTask;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -14,11 +9,9 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 
 import java.util.Map;
-import java.util.Random;
 
-import bd.com.ipay.ipayskeleton.Activities.SignupOrLoginActivity;
+import bd.com.ipay.ipayskeleton.Api.NotificationApi.CreateCustomNotificationAsyncTask;
 import bd.com.ipay.ipayskeleton.Model.CommunicationPOJO.Notification.FCMNotificationResponse;
-import bd.com.ipay.ipayskeleton.R;
 import bd.com.ipay.ipayskeleton.Utilities.AppInstance.AppInstanceUtilities;
 import bd.com.ipay.ipayskeleton.Utilities.ToasterAndLogger.Logger;
 
@@ -58,25 +51,7 @@ public class FCMListenerService extends FirebaseMessagingService {
     }
 
     private void createNotification(Context context, String title, String message, String imageUrl) {
-        int notificationID = new Random().nextInt();
-        Intent intent = new Intent(this, SignupOrLoginActivity.class);
-
-
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, notificationID, intent,
-                PendingIntent.FLAG_ONE_SHOT);
-
-        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent);
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(notificationID, notificationBuilder.build());
+        new CreateCustomNotificationAsyncTask(context, title,
+                message, imageUrl).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 }
