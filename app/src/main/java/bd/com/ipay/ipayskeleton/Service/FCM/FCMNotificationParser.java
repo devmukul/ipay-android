f5ca78be14cdf6a11ac470fedf54d34cc15d7f3f
maliha.sarwat <maliha.sarwat@ipay.com.bd>
2017-05-23 12:17:40 +0600

    IPAY-218 - Handled notification with no data payload

--- app/src/main/java/bd/com/ipay/ipayskeleton/Service/FCM/FCMNotificationParser.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/Service/FCM/FCMNotificationParser.java
@@ -11,26 +11,29 @@ public class FCMNotificationParser {
     public static void parseNotificationResponse(Context context, FCMNotificationResponse fcmNotificationResponse) {
         int serviceID = fcmNotificationResponse.getServiceId();
         boolean isReceiver = fcmNotificationResponse.isReceiver();
-        int status = fcmNotificationResponse.getNotificationData().getStatus();
 
-        switch (serviceID) {
-            case (Constants.TRANSACTION_HISTORY_SEND_MONEY):
-            case (Constants.TRANSACTION_HISTORY_MAKE_PAYMENT): {
-                MultipleBroadCastServiceIntent.sendBroadcastForPaymentOrSendMoney(context);
-                break;
-            }
-            case (Constants.TRANSACTION_HISTORY_REQUEST_MONEY):
-            case (Constants.TRANSACTION_HISTORY_REQUEST_PAYMENT): {
-                if (status == Constants.TRANSACTION_STATUS_PROCESSING) {
-                    MultipleBroadCastServiceIntent.sendBroadcastForPendingMoneyOrPaymentRequest(context, isReceiver);
+        if (fcmNotificationResponse.getNotificationData() != null) {
+            int status = fcmNotificationResponse.getNotificationData().getStatus();
 
-                } else if (status == Constants.TRANSACTION_STATUS_ACCEPTED) {
-                    MultipleBroadCastServiceIntent.sendBroadcastForAcceptedMoneyOrPaymentRequest(context);
-
-                } else if (status == Constants.TRANSACTION_STATUS_REJECTED || status == Constants.TRANSACTION_STATUS_CANCELLED) {
-                    MultipleBroadCastServiceIntent.sendBroadcastForCanceledOrRejectedRequest(context);
+            switch (serviceID) {
+                case (Constants.TRANSACTION_HISTORY_SEND_MONEY):
+                case (Constants.TRANSACTION_HISTORY_MAKE_PAYMENT): {
+                    MultipleBroadCastServiceIntent.sendBroadcastForPaymentOrSendMoney(context);
+                    break;
                 }
-                break;
+                case (Constants.TRANSACTION_HISTORY_REQUEST_MONEY):
+                case (Constants.TRANSACTION_HISTORY_REQUEST_PAYMENT): {
+                    if (status == Constants.TRANSACTION_STATUS_PROCESSING) {
+                        MultipleBroadCastServiceIntent.sendBroadcastForPendingMoneyOrPaymentRequest(context, isReceiver);
+
+                    } else if (status == Constants.TRANSACTION_STATUS_ACCEPTED) {
+                        MultipleBroadCastServiceIntent.sendBroadcastForAcceptedMoneyOrPaymentRequest(context);
+
+                    } else if (status == Constants.TRANSACTION_STATUS_REJECTED || status == Constants.TRANSACTION_STATUS_CANCELLED) {
+                        MultipleBroadCastServiceIntent.sendBroadcastForCanceledOrRejectedRequest(context);
+                    }
+                    break;
+                }
             }
         }
     }
