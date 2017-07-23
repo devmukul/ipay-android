479a202d8aec81f846c5ab77c6f665ca0866d8f6
MUKUL <mahbub.hasan@ipay.com.bd>
2017-07-23 14:20:22 +0600

    IPAY-184: Remove unnecessary comments

--- app/src/main/java/bd/com/ipay/ipayskeleton/Utilities/CustomCountDownTimer.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/Utilities/CustomCountDownTimer.java
@@ -4,25 +4,16 @@ import android.os.Handler;
 import android.os.Message;
 import android.os.SystemClock;
 
-/**
- * Created by MUKUL on 7/20/17.
- */
 
 public abstract class CustomCountDownTimer {
 
-    /**
-     * Millis since epoch when alarm should stop.
-     */
+    //Millis since epoch when alarm should stop.
     private final long mMillisInFuture;
-
-    /**
-     * The interval in millis that the user receives callbacks
-     */
+    //The interval in millis that the user receives callbacks
     private final long mCountdownInterval;
-
     private long mStopTimeInFuture;
-
     private long mNextTime;
+    private static final int MSG = 1;
 
     /**
      * @param millisInFuture    The number of millis in the future from the call
@@ -36,16 +27,12 @@ public abstract class CustomCountDownTimer {
         mCountdownInterval = countDownInterval;
     }
 
-    /**
-     * Cancel the countdown.
-     */
+    //Cancel the countdown.
     public final void cancel() {
         mHandler.removeMessages(MSG);
     }
 
-    /**
-     * Start the countdown.
-     */
+    //Start the countdown.
     public synchronized final CustomCountDownTimer start() {
         if (mMillisInFuture <= 0) {
             onFinish();
@@ -54,12 +41,10 @@ public abstract class CustomCountDownTimer {
 
         mNextTime = SystemClock.uptimeMillis();
         mStopTimeInFuture = mNextTime + mMillisInFuture;
-
         mNextTime += mCountdownInterval;
         mHandler.sendMessageAtTime(mHandler.obtainMessage(MSG), mNextTime);
         return this;
     }
-
 
     /**
      * Callback fired on regular interval.
@@ -68,18 +53,11 @@ public abstract class CustomCountDownTimer {
      */
     public abstract void onTick(long millisUntilFinished);
 
-    /**
-     * Callback fired when the time is up.
-     */
+    //Callback fired when the time is up.
     public abstract void onFinish();
-
-
-    private static final int MSG = 1;
-
 
     // handles counting down
     private Handler mHandler = new Handler() {
-
         @Override
         public void handleMessage(Message msg) {
             synchronized (CustomCountDownTimer.this) {
@@ -89,14 +67,12 @@ public abstract class CustomCountDownTimer {
                     onFinish();
                 } else {
                     onTick(millisLeft);
-
                     // Calculate next tick by adding the countdown interval from the original start time
                     // If user's onTick() took too long, skip the intervals that were already missed
                     long currentTime = SystemClock.uptimeMillis();
                     do {
                         mNextTime += mCountdownInterval;
                     } while (currentTime > mNextTime);
-
                     // Make sure this interval doesn't exceed the stop time
                     if(mNextTime < mStopTimeInFuture)
                         sendMessageAtTime(obtainMessage(MSG), mNextTime);
