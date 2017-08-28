371f1e96cda6f733e6949c33ce2ea2abdccd0d04
Md. Sajid Shahriar <sajid.shahriar@ipay.com.bd>
2017-08-28 12:30:45 +0600

    normal change in password

--- app/src/main/java/bd/com/ipay/ipayskeleton/LoginAndSignUpFragments/LoginFragments/LoginFragment.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/LoginAndSignUpFragments/LoginFragments/LoginFragment.java
@@ -97,7 +97,7 @@ public class LoginFragment extends Fragment implements HttpResponseListener {
 
         // Auto Login
         if (SharedPrefManager.ifContainsUserID() && Constants.DEBUG && Constants.AUTO_LOGIN) {
-            mPasswordEditText.setText("Itisfortest1");
+            mPasswordEditText.setText("qqqqqqq1");
             //           mUserNameEditText.setText("+8801677258077");
             attemptLogin();
         }
