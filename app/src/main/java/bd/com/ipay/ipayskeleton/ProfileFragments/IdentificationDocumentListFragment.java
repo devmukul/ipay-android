390a0ac7a09c255a5f11d86d483f32e826da59b8
Sourav Saha <sourav.saha@ipay.com.bd>
2017-10-16 14:38:56 +0600

    IPAY-1574: updated

--- app/src/main/java/bd/com/ipay/ipayskeleton/ProfileFragments/IdentificationDocumentListFragment.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/ProfileFragments/IdentificationDocumentListFragment.java
@@ -529,7 +529,7 @@ public class IdentificationDocumentListFragment extends ProgressFragment impleme
             private final Button mUploadButton;
             private final ImageView mPicker;
             private String documentTypeName;
-            private boolean cancel = false;
+            private boolean cancel;
 
             private View mDividerDocumentType;
 
@@ -709,6 +709,7 @@ public class IdentificationDocumentListFragment extends ProgressFragment impleme
                 mDocumentListRecyclerView.scrollToPosition(pos);
                 String documentID = mDocumentIdEditTextView.getText().toString();
                 String errorMessage;
+                cancel = false;
                 if (ProfileInfoCacheManager.isBusinessAccount())
                     errorMessage = InputValidator.isValidBusinessDocumentID(getActivity(), documentID, BUSINESS_DOCUMENT_TYPES[pos], pos);
                 else
