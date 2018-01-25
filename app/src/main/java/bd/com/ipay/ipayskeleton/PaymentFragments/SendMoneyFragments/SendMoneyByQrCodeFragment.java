1e6f93b1d0f87983b456baa162a78892d9db53d5
Sourav Saha <sourav.saha@ipay.com.bd>
2018-01-25 17:21:25 +0600

    IPAY-2762: Created new send money page for send money by qr code

---
+++ app/src/main/java/bd/com/ipay/ipayskeleton/PaymentFragments/SendMoneyFragments/SendMoneyByQrCodeFragment.java
@@ -0,0 +1,306 @@
+package bd.com.ipay.ipayskeleton.PaymentFragments.SendMoneyFragments;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.InputFilter;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import java.math.BigDecimal;
+
+import bd.com.ipay.ipayskeleton.Activities.DialogActivities.ContactPickerDialogActivity;
+import bd.com.ipay.ipayskeleton.Activities.PaymentActivities.SendMoneyActivity;
+import bd.com.ipay.ipayskeleton.Activities.PaymentActivities.SendMoneyReviewActivity;
+import bd.com.ipay.ipayskeleton.Api.GenericApi.HttpRequestGetAsyncTask;
+import bd.com.ipay.ipayskeleton.Api.HttpResponse.GenericHttpResponse;
+import bd.com.ipay.ipayskeleton.Api.HttpResponse.HttpResponseListener;
+import bd.com.ipay.ipayskeleton.BaseFragments.BaseFragment;
+import bd.com.ipay.ipayskeleton.CustomView.CustomContactsSearchView;
+import bd.com.ipay.ipayskeleton.Model.CommunicationPOJO.BusinessRuleAndServiceCharge.BusinessRule.BusinessRule;
+import bd.com.ipay.ipayskeleton.Model.CommunicationPOJO.BusinessRuleAndServiceCharge.BusinessRule.GetBusinessRuleRequestBuilder;
+import bd.com.ipay.ipayskeleton.R;
+import bd.com.ipay.ipayskeleton.Utilities.CacheManager.ProfileInfoCacheManager;
+import bd.com.ipay.ipayskeleton.Utilities.CacheManager.SharedPrefManager;
+import bd.com.ipay.ipayskeleton.Utilities.Constants;
+import bd.com.ipay.ipayskeleton.Utilities.ContactEngine;
+import bd.com.ipay.ipayskeleton.Utilities.ContactSearchHelper;
+import bd.com.ipay.ipayskeleton.Utilities.DecimalDigitsInputFilter;
+import bd.com.ipay.ipayskeleton.Utilities.InputValidator;
+import bd.com.ipay.ipayskeleton.Utilities.ToasterAndLogger.Toaster;
+import bd.com.ipay.ipayskeleton.Utilities.Utilities;
+
+public class SendMoneyByQrCodeFragment extends BaseFragment implements HttpResponseListener {
+
+    public static final int REQUEST_CODE_PERMISSION = 1001;
+
+    private final int PICK_CONTACT_REQUEST = 100;
+    private final int SEND_MONEY_REVIEW_REQUEST = 101;
+
+    private HttpRequestGetAsyncTask mGetBusinessRuleTask = null;
+
+    private Button buttonSend;
+    private ImageView buttonSelectFromContacts;
+    private ImageView buttonScanQRCode;
+    private CustomContactsSearchView mMobileNumberEditText;
+    private EditText mDescriptionEditText;
+    private EditText mAmountEditText;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_send_money_by_qr_code, container, false);
+        mMobileNumberEditText = (CustomContactsSearchView) v.findViewById(R.id.mobile_number);
+        mDescriptionEditText = (EditText) v.findViewById(R.id.description);
+        mAmountEditText = (EditText) v.findViewById(R.id.amount);
+        buttonScanQRCode = (ImageView) v.findViewById(R.id.button_scan_qr_code);
+        buttonSelectFromContacts = (ImageView) v.findViewById(R.id.select_receiver_from_contacts);
+        buttonSend = (Button) v.findViewById(R.id.button_send_money);
+
+        // Allow user to write not more than two digits after decimal point for an input of an amount
+        mAmountEditText.setFilters(new InputFilter[]{new DecimalDigitsInputFilter()});
+
+        mMobileNumberEditText.setCurrentFragmentTag(Constants.SEND_MONEY);
+
+        if (getActivity().getIntent().hasExtra(Constants.MOBILE_NUMBER)) {
+            mMobileNumberEditText.setText(getActivity().getIntent().getStringExtra(Constants.MOBILE_NUMBER));
+        }
+
+        buttonSelectFromContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ContactPickerDialogActivity.class);
+                intent.putExtra(Constants.IPAY_MEMBERS_ONLY, true);
+                startActivityForResult(intent, PICK_CONTACT_REQUEST);
+            }
+        });
+
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Utilities.isConnectionAvailable(getActivity())) {
+                    // For now, we are directly sending the money without going through any send money query
+                    // sendMoneyQuery();
+                    if (verifyUserInputs()) {
+                        launchReviewPage();
+                    }
+                } else if (getActivity() != null)
+                    Toaster.makeText(getActivity(), R.string.no_internet_connection, Toast.LENGTH_LONG);
+            }
+        });
+
+        buttonScanQRCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Utilities.performQRCodeScan(SendMoneyByQrCodeFragment.this, REQUEST_CODE_PERMISSION);
+
+            }
+        });
+
+        // Get business rule
+        attemptGetBusinessRule(Constants.SERVICE_ID_SEND_MONEY);
+
+        return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+       // Utilities.sendScreenTracker(mTracker, getString(R.string.screen_name_send_money_by_qr_c));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+
+        switch (requestCode) {
+            case REQUEST_CODE_PERMISSION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Utilities.initiateQRCodeScan(this);
+                } else {
+                    Toaster.makeText(getActivity(), R.string.error_camera_permission_denied, Toast.LENGTH_LONG);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_CONTACT_REQUEST && resultCode == Activity.RESULT_OK) {
+            String mobileNumber = data.getStringExtra(Constants.MOBILE_NUMBER);
+            if (mobileNumber != null)
+                mMobileNumberEditText.setText(mobileNumber);
+
+        } else if (requestCode == SEND_MONEY_REVIEW_REQUEST && resultCode == Activity.RESULT_OK) {
+            getActivity().finish();
+        } else if (resultCode == Activity.RESULT_OK && requestCode == IntentIntegrator.REQUEST_CODE) {
+            IntentResult scanResult = IntentIntegrator.parseActivityResult(
+                    requestCode, resultCode, data);
+            if (scanResult == null) {
+                return;
+            }
+            final String result = scanResult.getContents();
+            if (result != null) {
+                Handler mHandler = new Handler();
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (InputValidator.isValidNumber(result)) {
+                            mMobileNumberEditText.setText(ContactEngine.formatMobileNumberBD(result));
+                        } else if (getActivity() != null)
+                            Toaster.makeText(getActivity(), getResources().getString(
+                                    R.string.scan_valid_ipay_qr_code), Toast.LENGTH_SHORT);
+                    }
+                });
+            }
+        }
+    }
+
+    private boolean verifyUserInputs() {
+        mAmountEditText.setError(null);
+        mMobileNumberEditText.setError(null);
+
+        boolean cancel = false;
+        View focusView = null;
+        String errorMessage = null;
+
+        String mobileNumber = mMobileNumberEditText.getText().toString().trim();
+
+        if (SharedPrefManager.ifContainsUserBalance()) {
+            final BigDecimal balance = new BigDecimal(SharedPrefManager.getUserBalance());
+
+            if (TextUtils.isEmpty(mAmountEditText.getText())) {
+                errorMessage = getString(R.string.please_enter_amount);
+                focusView = mAmountEditText;
+                cancel = true;
+
+            } else {
+                final BigDecimal sendMoneyAmount = new BigDecimal(mAmountEditText.getText().toString());
+                if (sendMoneyAmount.compareTo(balance) > 0) {
+                    errorMessage = getString(R.string.insufficient_balance);
+                }
+                if (Utilities.isValueAvailable(SendMoneyActivity.mMandatoryBusinessRules.getMIN_AMOUNT_PER_PAYMENT())
+                        && Utilities.isValueAvailable(SendMoneyActivity.mMandatoryBusinessRules.getMAX_AMOUNT_PER_PAYMENT())) {
+
+                    final BigDecimal minimumSendMoneyAmount = SendMoneyActivity.mMandatoryBusinessRules.getMIN_AMOUNT_PER_PAYMENT();
+                    final BigDecimal maximumSendMoneyAmount = SendMoneyActivity.mMandatoryBusinessRules.getMAX_AMOUNT_PER_PAYMENT().min(balance);
+
+                    errorMessage = InputValidator.isValidAmount(getActivity(), sendMoneyAmount, minimumSendMoneyAmount, maximumSendMoneyAmount);
+                }
+            }
+        } else {
+            focusView = mAmountEditText;
+            errorMessage = getString(R.string.balance_not_available);
+            cancel = true;
+        }
+
+        if (errorMessage != null) {
+            focusView = mAmountEditText;
+            mAmountEditText.setError(errorMessage);
+            cancel = true;
+        } else if (!(mDescriptionEditText.getText().toString().trim().length() > 0)) {
+            focusView = mDescriptionEditText;
+            mDescriptionEditText.setError(getString(R.string.please_write_note));
+            cancel = true;
+        } else if (!InputValidator.isValidNumber(mobileNumber)) {
+            focusView = mMobileNumberEditText;
+            mMobileNumberEditText.setError(getString(R.string.please_enter_valid_mobile_number));
+            cancel = true;
+        } else if (ContactEngine.formatMobileNumberBD(mobileNumber).equals(ProfileInfoCacheManager.getMobileNumber())) {
+            focusView = mMobileNumberEditText;
+            mMobileNumberEditText.setError(getString(R.string.you_cannot_send_money_to_your_number));
+            cancel = true;
+        }
+
+        if (cancel) {
+            focusView.requestFocus();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void launchReviewPage() {
+
+        String receiver = mMobileNumberEditText.getText().toString().trim();
+        BigDecimal amount = new BigDecimal(mAmountEditText.getText().toString().trim());
+        String description = mDescriptionEditText.getText().toString().trim();
+
+        Intent intent = new Intent(getActivity(), SendMoneyReviewActivity.class);
+        intent.putExtra(Constants.AMOUNT, amount);
+        intent.putExtra(Constants.RECEIVER_MOBILE_NUMBER, ContactEngine.formatMobileNumberBD(receiver));
+        intent.putExtra(Constants.DESCRIPTION_TAG, description);
+        intent.putExtra(Constants.IS_IN_CONTACTS, new ContactSearchHelper(getActivity()).searchMobileNumber(receiver));
+
+        startActivityForResult(intent, SEND_MONEY_REVIEW_REQUEST);
+    }
+
+    private void attemptGetBusinessRule(int serviceID) {
+
+        if (mGetBusinessRuleTask != null) {
+            return;
+        }
+
+        String mUri = new GetBusinessRuleRequestBuilder(serviceID).getGeneratedUri();
+        mGetBusinessRuleTask = new HttpRequestGetAsyncTask(Constants.COMMAND_GET_BUSINESS_RULE,
+                mUri, getActivity(), this);
+
+        mGetBusinessRuleTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+    }
+
+    @Override
+    public void httpResponseReceiver(GenericHttpResponse result) {
+
+        if (result == null || result.getStatus() == Constants.HTTP_RESPONSE_STATUS_INTERNAL_ERROR
+                || result.getStatus() == Constants.HTTP_RESPONSE_STATUS_NOT_FOUND) {
+            if (getActivity() != null)
+                Toaster.makeText(getActivity(), R.string.service_not_available, Toast.LENGTH_SHORT);
+        } else if (result.getApiCommand().equals(Constants.COMMAND_GET_BUSINESS_RULE)) {
+
+            if (result.getStatus() == Constants.HTTP_RESPONSE_STATUS_OK) {
+
+                try {
+                    Gson gson = new Gson();
+
+                    BusinessRule[] businessRuleArray = gson.fromJson(result.getJsonString(), BusinessRule[].class);
+
+                    if (businessRuleArray != null) {
+
+                        for (BusinessRule rule : businessRuleArray) {
+                            if (rule.getRuleID().equals(Constants.SERVICE_RULE_SEND_MONEY_MAX_AMOUNT_PER_PAYMENT)) {
+                                SendMoneyActivity.mMandatoryBusinessRules.setMAX_AMOUNT_PER_PAYMENT(rule.getRuleValue());
+
+                            } else if (rule.getRuleID().equals(Constants.SERVICE_RULE_SEND_MONEY_MIN_AMOUNT_PER_PAYMENT)) {
+                                SendMoneyActivity.mMandatoryBusinessRules.setMIN_AMOUNT_PER_PAYMENT(rule.getRuleValue());
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    if (getActivity() != null)
+                        Toaster.makeText(getActivity(), R.string.service_not_available, Toast.LENGTH_LONG);
+                }
+
+            } else {
+                if (getActivity() != null)
+                    Toaster.makeText(getActivity(), R.string.service_not_available, Toast.LENGTH_LONG);
+            }
+
+            mGetBusinessRuleTask = null;
+        }
+    }
+}
