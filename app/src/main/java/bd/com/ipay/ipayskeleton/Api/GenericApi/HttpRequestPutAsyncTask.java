0c94198edae237785d6d0c23c0af982ec980d307
Sourav Saha <sourav.saha@ipay.com.bd>
2018-04-08 11:55:54 +0600

    IPAY-3803: Work on going

--- app/src/main/java/bd/com/ipay/ipayskeleton/Api/GenericApi/HttpRequestPutAsyncTask.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/Api/GenericApi/HttpRequestPutAsyncTask.java
@@ -3,14 +3,17 @@ package bd.com.ipay.ipayskeleton.Api.GenericApi;
 import android.content.Context;
 
 import bd.com.ipay.ipayskeleton.Api.HttpResponse.HttpResponseListener;
+import bd.com.ipay.ipayskeleton.Utilities.Constants;
 import bd.com.ipay.ipayskeleton.Utilities.ToasterAndLogger.Logger;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
 
 public class HttpRequestPutAsyncTask extends HttpRequestAsyncTask {
 
     private final String mJsonString;
 
     public HttpRequestPutAsyncTask(String API_COMMAND, String mUri, String mJsonString,
-                                     Context mContext, HttpResponseListener listener) {
+                                   Context mContext, HttpResponseListener listener) {
         super(API_COMMAND, mUri, mContext, listener);
         this.mJsonString = mJsonString;
     }
@@ -25,6 +28,20 @@ public class HttpRequestPutAsyncTask extends HttpRequestAsyncTask {
         if (mJsonString != null)
             Logger.logW("json", mJsonString);
 
-        return mJsonString;
+        MediaType JSON
+                = MediaType.parse("application/json; charset=utf-8");
+        RequestBody requestBody = RequestBody.create(JSON, mJsonString);
+        if (mJsonString != null)
+            Logger.logW("json", mJsonString);
+        okhttp3.Request request = new okhttp3.Request.Builder().
+                header(Constants.USER_AGENT, Constants.USER_AGENT_MOBILE_ANDROID)
+                .header("Accept", "application/json")
+                .header("Content-type", "application/json")
+                .header(Constants.TOKEN, "")
+                .header(Constants.OPERATING_ON_ACCOUNT_ID, "")
+                .put(requestBody)
+                .url(mUri)
+                .build();
+        return request;
     }
 }
