cc623efc7f991d73187981d4da45004ad72c8201
maliha.sarwat <maliha.sarwat@ipay.com.bd>
2018-04-10 11:15:56 +0600

    IPAY-3870 [Android] - Verified Personal user tries to Make Payment to Unverified Business user. The error message "Receiver must be a verified Business member" should pop up before entering the PIN number.

--- app/src/main/java/bd/com/ipay/ipayskeleton/PaymentFragments/MakePaymentFragments/MakePaymentFragment.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/PaymentFragments/MakePaymentFragments/MakePaymentFragment.java
@@ -61,7 +61,6 @@ import bd.com.ipay.ipayskeleton.Model.CommunicationPOJO.Profile.BasicInfo.UserAd
 import bd.com.ipay.ipayskeleton.QRScanner.BarcodeCaptureActivity;
 import bd.com.ipay.ipayskeleton.R;
 import bd.com.ipay.ipayskeleton.Utilities.BusinessRuleConstants;
-import bd.com.ipay.ipayskeleton.Utilities.CacheManager.ACLManager;
 import bd.com.ipay.ipayskeleton.Utilities.CacheManager.ProfileInfoCacheManager;
 import bd.com.ipay.ipayskeleton.Utilities.CacheManager.SharedPrefManager;
 import bd.com.ipay.ipayskeleton.Utilities.Constants;
@@ -70,7 +69,6 @@ import bd.com.ipay.ipayskeleton.Utilities.DecimalDigitsInputFilter;
 import bd.com.ipay.ipayskeleton.Utilities.DialogUtils;
 import bd.com.ipay.ipayskeleton.Utilities.InputValidator;
 import bd.com.ipay.ipayskeleton.Utilities.MyApplication;
-import bd.com.ipay.ipayskeleton.Utilities.ServiceIdConstants;
 import bd.com.ipay.ipayskeleton.Utilities.ToasterAndLogger.Toaster;
 import bd.com.ipay.ipayskeleton.Utilities.TwoFactorAuthConstants;
 import bd.com.ipay.ipayskeleton.Utilities.Utilities;
