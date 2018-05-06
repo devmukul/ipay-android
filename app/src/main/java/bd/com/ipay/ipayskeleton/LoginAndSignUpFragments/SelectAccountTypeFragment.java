10832221237689bb3cd959b58c142df33e23b0de
maliha.sarwat <maliha.sarwat@ipay.com.bd>
2018-05-06 12:05:15 +0600

    IPAY-4069 otp is not placing in the OTP filed automatically from SMS init

--- app/src/main/java/bd/com/ipay/ipayskeleton/LoginAndSignUpFragments/SelectAccountTypeFragment.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/LoginAndSignUpFragments/SelectAccountTypeFragment.java
@@ -1,14 +1,27 @@
 package bd.com.ipay.ipayskeleton.LoginAndSignUpFragments;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import bd.com.ipay.ipayskeleton.Activities.SignupOrLoginActivity;
+import bd.com.ipay.ipayskeleton.Api.ContactApi.GetContactsAsyncTask;
 import bd.com.ipay.ipayskeleton.BaseFragments.BaseFragment;
 import bd.com.ipay.ipayskeleton.R;
+import bd.com.ipay.ipayskeleton.Utilities.CacheManager.ACLManager;
+import bd.com.ipay.ipayskeleton.Utilities.ServiceIdConstants;
+import bd.com.ipay.ipayskeleton.Utilities.ToasterAndLogger.Logger;
 import bd.com.ipay.ipayskeleton.Utilities.Utilities;
 
 public class SelectAccountTypeFragment extends BaseFragment {
@@ -16,6 +29,8 @@ public class SelectAccountTypeFragment extends BaseFragment {
     private Button buttonAccountTypePersonal;
     private Button buttonAccountTypeBusiness;
     private View v;
+
+    private static final int REQUEST_CODE_PERMISSION = 1001;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -47,5 +62,44 @@ public class SelectAccountTypeFragment extends BaseFragment {
         super.onResume();
         getActivity().setTitle(R.string.title_select_account_type_page);
         Utilities.sendScreenTracker(mTracker, getString(R.string.screen_name_select_account_type));
+    }
+
+    private void attemptRequestForPermission() {
+        String[] requiredPermissions = {Manifest.permission.READ_SMS};
+
+        List<String> permissionsToRequest = new ArrayList<>();
+        for (String permission : requiredPermissions) {
+            if (ContextCompat.checkSelfPermission(getActivity(), permission) != PackageManager.PERMISSION_GRANTED) {
+                permissionsToRequest.add(permission);
+            }
+        }
+
+        if (!permissionsToRequest.isEmpty()) {
+            ActivityCompat.requestPermissions(getActivity(), permissionsToRequest.toArray(new String[permissionsToRequest.size()]),
+                    REQUEST_CODE_PERMISSION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_CODE_PERMISSION:
+                for (int i = 0; i < permissions.length; i++) {
+                    Logger.logW(permissions[i], grantResults[i] + "");
+
+                    if (permissions[i].equals(Manifest.permission.READ_CONTACTS)) {
+                        if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                            if (ACLManager.hasServicesAccessibility(ServiceIdConstants.GET_CONTACTS))
+                                new GetContactsAsyncTask(getActivity()).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+                        }
+                    } else if (permissions[i].equals(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                      //  startLocationCollection();
+                    }
+                }
+
+                break;
+        }
     }
 }
