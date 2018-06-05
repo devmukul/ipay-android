c8d27636855b9c15fdc56604a9173bd24931a0c6
maliha.sarwat <maliha.sarwat@ipay.com.bd>
2018-06-05 12:55:41 +0600

    Add Money by card  in on board crash issue for business rule cache is fixed

--- app/src/main/java/bd/com/ipay/ipayskeleton/Utilities/MyApplication.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/Utilities/MyApplication.java
@@ -66,7 +66,7 @@ public class MyApplication extends MultiDexApplication implements HttpResponseLi
         ACLManager.initialize(this);
         TokenManager.initialize(this);
         Intercom.initialize(this, Constants.INTERCOM_ANDROID_SDK_KEY, Constants.INTERCOM_API_KEY);
-        BusinessRuleCacheManager.initialize(getApplicationContext());
+        BusinessRuleCacheManager.initialize(this);
         setDefaultBusinessRules();
         Utilities.resetIntercomInformation();
         sAnalytics = GoogleAnalytics.getInstance(this);
