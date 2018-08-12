e44dd5a7004d28beadaa695c8b3852782f72bc66
Sourav Saha <sourav.saha@ipay.com.bd>
2018-08-12 11:12:51 +0600

    IPAY-4552: Webview back button navigation corrected

--- app/src/main/java/bd/com/ipay/ipayskeleton/Activities/WebViewActivity.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/Activities/WebViewActivity.java
@@ -1,5 +1,6 @@
 package bd.com.ipay.ipayskeleton.Activities;
 
+import android.app.ActivityManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,6 +10,8 @@ import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import java.util.List;
 
 import bd.com.ipay.ipayskeleton.R;
 
@@ -33,41 +36,37 @@ public class WebViewActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if (getIntent() != null) {
-            if (getIntent().hasExtra("sourceActivity")) {
-                if (webView.canGoBack()) {
-                    webView.goBack();
-                } else {
-                    super.onBackPressed();
-                }
-            } else {
-                if (uriString != null && !uriString.isEmpty()) {
-                    if (uriString.toLowerCase().contains("promotion")) {
-                        Intent intent = new Intent(this, HomeActivity.class);
-                        startActivity(intent);
-                    } else {
-                        if (webView.canGoBack()) {
-                            webView.goBack();
-                        } else {
-                            super.onBackPressed();
-                        }
-                    }
-                } else {
-                    if (webView.canGoBack()) {
-                        webView.goBack();
-                    } else {
-                        super.onBackPressed();
-                    }
-                }
-
-            }
+        if (webView.canGoBack()) {
+            webView.goBack();
         } else {
-            if (webView.canGoBack()) {
-                webView.goBack();
+            if (isLaunchedFromHomeScreen()) {
+                Intent intent = new Intent(WebViewActivity.this, HomeActivity.class);
+                startActivity(intent);
             } else {
-                super.onBackPressed();
+                finish();
             }
         }
-        finish();
+    }
+
+    /*We need to check if promotional notification is launched from home screen of android device,
+    in that case we will try redirect to wallet page on back button press. otherwise back press will just finish
+     the web view activity and behave normally
+    */
+
+    private boolean isLaunchedFromHomeScreen() {
+        try {
+            ActivityManager mngr = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+
+            List<ActivityManager.RunningTaskInfo> taskList = mngr.getRunningTasks(10);
+
+            if (taskList.get(0).numActivities == 1 &&
+                    taskList.get(0).topActivity.getClassName().equals(this.getClass().getName())) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
