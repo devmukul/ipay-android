9466b2c4d7d4a7f87c62c1764ef25f1b8492a62b
Sourav Saha <sourav.saha@ipay.com.bd>
2018-10-16 09:24:51 +0600

    IPAY-5362: Send money v3 removed .

--- app/src/main/java/bd/com/ipay/ipayskeleton/Model/CommunicationPOJO/BusinessRuleAndServiceCharge/BusinessRule/GetBusinessRuleRequestBuilder.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/Model/CommunicationPOJO/BusinessRuleAndServiceCharge/BusinessRule/GetBusinessRuleRequestBuilder.java
@@ -16,8 +16,7 @@ public class GetBusinessRuleRequestBuilder {
 
 	private void generateUri(int serviceId) {
 		final Uri uri;
-		if (serviceId == ServiceIdConstants.UTILITY_BILL_PAYMENT || serviceId == ServiceIdConstants.MAKE_PAYMENT ||
-				serviceId == ServiceIdConstants.SEND_MONEY) {
+		if (serviceId == ServiceIdConstants.UTILITY_BILL_PAYMENT || serviceId == ServiceIdConstants.MAKE_PAYMENT) {
 			uri = Uri.parse(Constants.BASE_URL_SM + Constants.URL_BUSINESS_RULE_V2 + "/" + serviceId);
 		} else {
 			uri = Uri.parse(Constants.BASE_URL_SM + Constants.URL_BUSINESS_RULE + "/" + serviceId);
