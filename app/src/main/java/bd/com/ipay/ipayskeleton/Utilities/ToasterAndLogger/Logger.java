dd7757f0337fc020ee9b81598d90e42b1cc51815
Md. Sajid Shahriar <sajid.shahriar@ipay.com.bd>
2018-11-01 09:37:09 +0600

    Log bug fixed

--- app/src/main/java/bd/com/ipay/ipayskeleton/Utilities/ToasterAndLogger/Logger.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/Utilities/ToasterAndLogger/Logger.java
@@ -7,27 +7,27 @@ import bd.com.ipay.ipayskeleton.BuildConfig;
 public class Logger {
 
     public static void logD(String tag, String message) {
-        if (!BuildConfig.DEBUG)
+        if (BuildConfig.DEBUG)
             Log.d(tag, message);
     }
 
     public static void logD(String tag, String message, Exception e) {
-        if (!BuildConfig.DEBUG)
+        if (BuildConfig.DEBUG)
             Log.d(tag, message, e);
     }
 
     public static void logI(String tag, String message) {
-        if (!BuildConfig.DEBUG)
+        if (BuildConfig.DEBUG)
             Log.i(tag, message);
     }
 
     public static void logE(String tag, String message) {
-        if (!BuildConfig.DEBUG)
+        if (BuildConfig.DEBUG)
             Log.e(tag, message);
     }
 
     public static void logW(String tag, String message) {
-        if (!BuildConfig.DEBUG)
+        if (BuildConfig.DEBUG)
             Log.w(tag, message);
     }
 }
