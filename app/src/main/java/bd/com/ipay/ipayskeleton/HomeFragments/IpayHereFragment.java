f36b731d261d32d5aa5cf05fcd66f253749605ed
MUKUL <mahbub.hasan@ipay.com.bd>
2019-01-07 15:57:01 +0600

    Clear list after loading

--- app/src/main/java/bd/com/ipay/ipayskeleton/HomeFragments/IpayHereFragment.java
+++ app/src/main/java/bd/com/ipay/ipayskeleton/HomeFragments/IpayHereFragment.java
@@ -127,6 +127,7 @@ public class IpayHereFragment extends ProgressFragment implements PlaceSelection
     }
 
     private void startLocationUpdates() {
+        clearListAfterLoading = true;
         if(!ifPermossionGranted()){
             requestPermissions( new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
         } else {
