f4c6da85affbbea224cf94b5f52f017c6eb14a8c
Sourav Saha <sourav.saha@ipay.com.bd>
2019-02-14 15:10:07 +0600

    Small language related change

---
+++ app/src/main/java/bd/com/ipay/ipayskeleton/PaymentFragments/UtilityBillFragments/DescoBillSuccessFragment.java
@@ -0,0 +1,34 @@
+package bd.com.ipay.ipayskeleton.PaymentFragments.UtilityBillFragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import bd.com.ipay.ipayskeleton.PaymentFragments.IPayAbstractTransactionSuccessFragment;
+import bd.com.ipay.ipayskeleton.R;
+import bd.com.ipay.ipayskeleton.Utilities.Constants;
+
+public class DescoBillSuccessFragment extends IPayAbstractTransactionSuccessFragment {
+
+    private Number billAmount;
+    private String descoAccountId;
+    private String billNumber;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            billAmount = (Number) getArguments().getSerializable(Constants.TOTAL_AMOUNT);
+            descoAccountId = getArguments().getString(Constants.ACCOUNT_ID, "");
+            billNumber = getArguments().getString(Constants.BILL_NUMBER, "");
+        }
+    }
+
+    @Override
+    protected void setupViewProperties() {
+        setTransactionSuccessMessage(getStyledTransactionDescription(R.string.pay_bill_success_message, billAmount));
+        setSuccessDescription(getString(R.string.pay_bill_success_description));
+        setName(billNumber);
+        setUserName(descoAccountId);
+        setReceiverImage(R.drawable.desco);
+    }
+}
